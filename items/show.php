eb8377d6be382fd6d933caec75d2ea7e0a4387eb
kalbers <ken.albers@gmail.com>
2015-04-03 16:53:38 -0400

    Changes to one column layout. Moves file to top, metadata, etc.

--- items/show.php
+++ items/show.php
@@ -3,42 +3,17 @@
 
 <div id="primary">
     <h2><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h2>
+
+    <?php if (metadata('item', 'has files')): ?>
+        <div id="itemfiles" class="element">
+            <h3><?php echo __('Files'); ?></h3>
+            <div class="element-text"><?php echo files_for_item(array('imageSize' => 'fullsize')); ?></div>
+        </div>
+    <?php endif; ?>
         
     <div class="element">
         <?php echo metadata('item', array('Dublin Core', 'Description')); ?>
     </div>
-
-    <div class="element">
-        <h5>Date</h5>
-        <div class="element-text">
-            <?php echo metadata('item', array('Dublin Core', 'Date')); ?>
-        </div>
-    </div>
-
-    <div class="element">
-        <h5>Creator</h5>
-        <div class="element-text">
-            <?php echo metadata('item', array('Dublin Core', 'Creator')); ?>
-        </div>
-    </div>
-
-    <div class="element">
-        <h5>Subject</h5>
-        <div class="element-text">
-            <?php echo metadata('item', array('Dublin Core', 'Subject')); ?>
-        </div>     
-    </div>
-
-    <?php if($itemType == 'Document' || $itemType == 'Diary'): ?>
-        <?php if (metadata('item', 'has files')): ?>
-            <div id="itemfiles" class="element">
-                <h3><?php echo __('Files'); ?></h3>
-                <div class="element-text"><?php echo files_for_item(); ?></div>
-            </div>
-        <?php endif; ?>
-    <?php endif; ?>
-        
-    
     
     <button class="more-info">More information about this item</button>
     <div id="secondary-metadata">
@@ -54,24 +29,6 @@
 </div> <!-- end primary -->
 
 <div id="secondary">
-    
-    <?php $location = get_db()->getTable('Location')->findLocationByItem($item, true); ?>
-    <?php if($location): ?>
-    <div class="map-show">
-        <?php echo $this->itemGoogleMap($item, $width = '100%', $height = '250px');?>    
-    </div>
-    <?php endif; ?>
-
-    <!-- The following returns all of the files associated with an item. -->
-    <?php if($itemType != 'Document' && $itemType != 'Diary'): ?>
-        <?php if (metadata('item', 'has files')): ?>
-            <div id="itemfiles" class="element">
-                <h3><?php echo __('Files'); ?></h3>
-                <div class="element-text"><?php echo files_for_item(); ?></div>
-            </div>
-        <?php endif; ?>
-    <?php endif; ?>
-
 </div>
 
 <nav>
