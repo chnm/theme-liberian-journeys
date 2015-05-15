5c21fdb0c42d0bbde3cf798671bd447b2a8287a1
kalbers <ken.albers@gmail.com>
2015-05-15 13:25:54 -0400

    Alter link for map page in top nav.

--- common/header.php
+++ common/header.php
@@ -43,7 +43,7 @@
             <nav id="top-nav">
                 <?php 
                 $navArray = array();
-                $navArray[] = array('label' => 'Map', 'uri' => url('geolocation/map/browse'), 'class' => 'map');
+                $navArray[] = array('label' => 'Map', 'uri' => url('map'), 'class' => 'map');
                 $navArray[] = array('label' => 'Exhibits', 'uri' => url('exhibits'), 'class' => 'exhibits');
                 $navArray[] = array('label' => 'Collections', 'uri' => url('items/browse?type=6'), 'class' => 'collections');
                 //$navArray[] = array('label' => 'Stories', 'uri' => url('items/browse?contributed=1'), 'class' => 'stories');
@@ -58,7 +58,7 @@
             <div id="mobile-nav">
                 <?php 
                 $navArray = array();
-                $navArray[] = array('label' => 'Map', 'uri' => url('geolocation/map/browse'), 'class' => 'map');
+                $navArray[] = array('label' => 'Map', 'uri' => url('map'), 'class' => 'map');
                 $navArray[] = array('label' => 'Exhibits', 'uri' => url('exhibits'), 'class' => 'exhibits');
                 $navArray[] = array('label' => 'Collections', 'uri' => url('items/browse?type=6'), 'class' => 'collections');
                 //$navArray[] = array('label' => 'Stories', 'uri' => url('items/browse?contributed=1'), 'class' => 'stories');
