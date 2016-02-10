ea3545241ccb570c0cf0c1708f5051d13fb4583e
Ken Albers <ken.albers@gmail.com>
2016-02-10 13:51:50 -0500

    Adds google analytics code

--- common/header.php
+++ common/header.php
@@ -30,6 +30,17 @@
     <!-- JavaScripts -->
     <?php queue_js_file(array('vendor/modernizr', 'globals')); ?>
     <?php echo head_js(); ?>
+
+    <script>
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ ga('create', 'UA-73570479-1', 'auto');
+ ga('send', 'pageview');
+
+</script>
 </head>
 
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
